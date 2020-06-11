9a72f46149a11f786b22b035c234a2611c16dc41
hansenjl <jenn.leigh.hansen@gmail.com>
2020-06-11 09:00:23 -0700

    finish sinatra study group

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,25 @@
+class ApplicationController < Sinatra::Base
+
+    get '/' do
+
+        Movie.all.map do |m|
+            m.name
+        end.join("$$")
+
+        <<-HTML
+            <!DOCTYPE html>
+            <html>
+                <head>
+                    <title>Flatiron</title>
+                </head>
+                <body>
+                     <h1>Hello!</h1>
+                     <h2>Have a great Thursday</h2>
+                     <p>this is our first sinatra app</p>
+                </body>
+            </html>
+
+        HTML
+
+    end
+end
